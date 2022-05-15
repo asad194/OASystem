de64bde5f4a0128dea9d302af057c3700c039634
asad194 <a19653399>
2022-05-15 20:37:36 +0900

    first commit

---
+++ src/main/java/org/example/OASystem/OaSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.OASystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaSystemApplication.class, args);
+	}
+
+}
