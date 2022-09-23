c636596e6d959d1b36f90c0df218953883c3b3f8
asad194 <asdddd194@gmail.com>
2022-09-23 19:46:24 +0900

    文字送信・文字表示機能作成

--- src/main/java/org/example/OASystem/top/TopController.java
+++ src/main/java/org/example/OASystem/top/TopController.java
@@ -1,7 +1,10 @@
 package org.example.OASystem.top;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /***
  * HTTPリクエストを受け付けるクラス＝コントローラー
@@ -23,4 +26,17 @@ public class TopController {
 		return "top";
 	}
 	
+	// PostMappingアノテーション＝受け付けるURLを引数に指定すると、POSTを受け取る。以下の場合、http://localhost:8080/topへのPOSTリクエストを受け取れる。
+	@PostMapping("/top")
+	// RequestParamアノテーション＝画面で入力された値を受け取るためのアノテーション。HTMLのname属性と一致するように引数を指定する。
+	public String postRequest(@RequestParam("text1") String str, Model model) {
+		
+		// 画面から受け取った文字列をModelに登録
+		model.addAttribute("sample", str);
+		// ↑Modelクラス：別画面に値を渡すためのクラス。addAttributeメソッド＝(キー名, 値)と引数を指定すると、Modelクラスに値が登録される。
+		// ModelクラスはControllerからViewへ変数を渡すためのもの。今回の場合、TopController→Model→response.htmlと、strが渡されている。
+		
+		// response.htmlに画面遷移
+		return "top/response";
+	}
 }
